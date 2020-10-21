dd12089ab829e6b8cded9bed6993e1b0f11a2b94
Gene Liverman <gene.liverman+06301990@puppet.com>
2020-10-21 16:21:08 -0400

    Add debug output

--- steps/event-send/step.py
+++ steps/event-send/step.py
@@ -3,6 +3,7 @@
 from sys import exit
 from pdpyras import EventsAPISession
 from relay_sdk import Interface, Dynamic as D
+import json
 
 relay = Interface()
 
@@ -17,10 +18,6 @@ session = EventsAPISession(token)
 # python SDK yet
 eventType = relay.get(D.type)
 dedup_key = relay.get(D.dedup_key)
-payload = None
-custom_details = relay.get(D.custom_details)
-images = None
-links = relay.get(D.links)
 
 if eventType is None:
     exit("A type is required for sending events, but none was set")
@@ -28,13 +25,27 @@ elif eventType == "trigger":
     summary = relay.get(D.summary)
     source = relay.get(D.source)
     severity = relay.get(D.severity)
+    payload = None
+    custom_details = relay.get(D.custom_details)
+    images = None
+    links = relay.get(D.links)
+
+    print("summary: {}".format(summary))
+    print("source: {}".format(source))
+    print("dedup_key: {}".format(json.dumps(dedup_key, indent=2)))
+    print("severity: {}".format(severity))
+    print("custom_details: {}".format(json.dumps(custom_details, indent=2)))
+    print("links: {}".format(json.dumps(links, indent=2)))
+
     if summary is None:
         exit("A summary is required for event type trigger, but none was set")
     if source is None:
         exit("A source is required for event type trigger, but none was set")
     if severity is None:
         exit("A severity is required for event type trigger, but none was set")
-    response = session.trigger(summary, source, dedup_key, severity, payload, custom_details, images, links)
+    response = session.trigger(
+        summary, source, dedup_key, severity,
+        payload, custom_details, images, links)
 elif eventType == "acknowledge":
     if dedup_key is None:
         exit("A dedup_key is required for event type acknowledge, but none was set")
